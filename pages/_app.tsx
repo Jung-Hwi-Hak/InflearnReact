efdff8693aceae2f4cf47e771fbadc80c17d5a74
정휘학 <gnlgrkdl@danusys.com>
2023-06-16 16:33:09 +0900

    section14 - _app.tsx 코드에 Apollo 세팅코드 분할 작업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,17 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../src/components-example/commons/layout";
+import ApolloSetting from "../src/components-example/commons/apollo";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  const client = new ApolloClient({
-    uri: "http://backend-example.codebootcamp.co.kr/graphql",
-    // uri: "http://backend-practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), // 컴퓨터 메모리에 백엔드에서 받아온 데이터 임시로 저장해 놓기
-  });
-
   return (
     <div>
       <div>====여기는 _app.js 컴포넌트 입니다.====</div>
-      <ApolloProvider client={client}>
+      <ApolloSetting>
         <Layout>
           <Component />
         </Layout>
-      </ApolloProvider>
+      </ApolloSetting>
       <div>====여기는 _app.js 컴포넌트 입니다.====</div>
     </div>
   );
