2e2b15ec89b3559addca66aba20d8bc23a20943d
정휘학 <gnlgrkdl@danusys.com>
2023-07-09 21:30:43 +0900

    accessToken 을 localStorage에 저장 실습

--- src/components-example/commons/apollo/index.tsx
+++ src/components-example/commons/apollo/index.tsx
@@ -7,6 +7,7 @@ import {
 import { useRecoilState } from "recoil";
 import { createUploadLink } from "apollo-upload-client";
 import { accessTokenState } from "../../../commons/stores";
+import { useEffect } from "react";
 
 // 서버데이터
 const GLOBAL_STATE = new InMemoryCache();
@@ -16,7 +17,28 @@ interface IApolloSettingProps {
 }
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
-  const [accessToken] = useRecoilState(accessTokenState);
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  // TODO 브라우저일 경우에만 실행하기 - localStorage.getItem()
+  // ? 첫번째 방법 - process.browser
+  // if (process.browser) {
+  //   const localStorageAccessToken = localStorage.getItem("accessToken");
+  //   setAccessToken(localStorageAccessToken ?? "");
+  // } else {
+  //   console.log("지금은 프론트엔드 서버 - (next.js - yarn dev 실행중)");
+  // }
+  // ? 두번째 방법 - typeof window
+  // if (typeof window !== "undefined") {
+  //   const localStorageAccessToken = localStorage.getItem("accessToken");
+  //   setAccessToken(localStorageAccessToken ?? "");
+  // } else {
+  //   console.log("지금은 프론트엔드 서버 - (next.js - yarn dev 실행중)");
+  // }
+  // ? 세번째 방법 - useEffect()
+  useEffect(() => {
+    const localStorageAccessToken = localStorage.getItem("accessToken");
+    setAccessToken(localStorageAccessToken ?? "");
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: "http://backend-practice.codebootcamp.co.kr/graphql",
